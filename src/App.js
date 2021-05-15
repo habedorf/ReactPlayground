bc86a9dc508624b90a14a1dd4759099763ada5aa
Robin <you@example.com>
2021-05-15 14:49:45 +0200

    font bold

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ setSearchTerm(event.target.value);
 };
 return (
 <div className={"container mx-5"}>
-<h1 className={"my-5"}>My Hacker Stories</h1>
+<h1 className={"my-5 text-2xl font-bold"}>My Hacker Stories</h1>
 <label htmlFor="search">Search: </label>
 <input id="search" type="text" onChange={handleChange} />
 <p>
