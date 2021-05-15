2e938db08d56919140ba2c1a216f442f65070779
Robin <you@example.com>
2021-05-15 13:03:15 +0200

    ok

---
+++ src/counter.js
@@ -0,0 +1,46 @@
+import * as React from 'react';
+
+
+const Counter = () =>{
+
+    const [count, setCount] = React.useState(0);
+ 
+    const handleIncrease = () => {
+      setCount(count + 1);
+    };
+   
+    const handleDecrease = () => {
+      setCount(count - 1);
+    };
+   
+    return (
+      <div>
+        Count: {count}
+        <hr />
+        <div>
+          <button type="button" onClick={handleIncrease}>
+            Increase
+          </button>
+          <button type="button" onClick={handleDecrease}>
+            Decrease
+          </button>
+        </div>
+      </div>
+    );
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default Counter;
